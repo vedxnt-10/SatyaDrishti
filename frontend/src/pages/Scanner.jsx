33f1171a69b48fef7879366a801264883cabfcb2
Vedant <vishambharivedant00@gmail.com>
2026-09-19 13:20:48 +0530

    fix: import useEffect in Scanner.jsx

--- frontend/src/pages/Scanner.jsx
+++ frontend/src/pages/Scanner.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import UploadZone from '../components/UploadZone';
 import ProcessingSteps from '../components/ProcessingSteps';
 import ResultsPanel from '../components/ResultsPanel';
